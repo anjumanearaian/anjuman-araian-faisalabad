e7e3654c3cb93f938a5f2ce7a7c61f8174337fb5
Anjuman-E- Araian Faisalabad <anjumanearaianfaisalabad@gmail.com>
2026-09-07 02:16:12 +0500

    Refactor import and default export of backend module

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,8 @@
-import app from "../backend/index";
+import backendModule from "../backend/index.js";
+
+const app =
+  typeof backendModule === "function"
+    ? backendModule
+    : (backendModule as any).default;
 
 export default app;
