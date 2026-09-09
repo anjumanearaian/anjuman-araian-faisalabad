e75b4da3516d73df7ee3104ac15dd9dba16b6529
Anjuman-E- Araian Faisalabad <anjumanearaianfaisalabad@gmail.com>
2026-09-09 07:07:47 +0500

    Load business spotlight styles

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -8,6 +8,7 @@ import ServicesSection from "../components/home/ServicesSection";
 import StatisticsSection from "../components/home/StatisticsSection";
 import EventsSection from "../components/home/EventsSection";
 import "../../styles/homepage-final.css";
+import "../../styles/business-spotlight.css";
 
 export function HomePage(){
  return <>
