35b48b41bb639b78e3a7bf724040100e722c6733
Anjuman-E- Araian Faisalabad <anjumanearaianfaisalabad@gmail.com>
2026-09-09 05:04:35 +0500

    Stay within Vercel Hobby function limit

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import { createRequire } from "node:module";
-
-const require = createRequire(import.meta.url);
-const backendModule = require("../backend/dist/index.js");
-const app = backendModule.default ?? backendModule;
-
-export default app;
